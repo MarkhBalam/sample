535822fcbe71dc6c40b2ac45004d5f6f2d37edb9
mugishabalam4 <mugishabalam4@gmail.com>
2024-03-20 23:47:01 +0300

    template modifications

--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-
-def home_view(request):
-    # Define your product data
-    products = [
-        {"name": "Watch", "image": "watch.jpg", "price": 99.99, "discount": 10, "rating": 4},
-        {"name": "Shoes", "image": "shoes.jpg", "price": 49.99, "discount": 5, "rating": 3},
-        {"name": "Shirt", "image": "shirt.jpg", "price": 29.99, "discount": 15, "rating": 2},
-        # Add more products here
-    ]
-    
-    return render(request, "home.html", {"products": products})
