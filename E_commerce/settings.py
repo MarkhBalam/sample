42a6164f52b6666095b09438b561d8ee9e884bec
Namanya Rinah <namanyarinah4@gmail.com>
2024-03-23 00:07:12 +0300

    chaged afew styles in orderstatus.html

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'orderstatus'
-     'ADD',
+    'orderstatus',
+    'ADD',
     'login',
     'django.contrib.admin',
     'django.contrib.auth',
