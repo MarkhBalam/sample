a9518c437e29c2ab4ca459f9b1b2d1d7b896014c
mugishabalam4 <mugishabalam4@gmail.com>
2024-03-24 18:45:59 +0300

    url modifications

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,8 +124,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Path to your project's static files
-]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
