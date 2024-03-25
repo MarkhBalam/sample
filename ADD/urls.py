7918a74ff64dfdb3c6434c7d0b9c77fa7a063a5e
mugishabalam4 <mugishabalam4@gmail.com>
2024-03-25 11:28:11 +0300

    deleted add product app

--- ADD/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('order/', views.order, name="order" ),
-    path('add/', views.add, name="add")
-]
--- ADD/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def add(request):
-      return render(request, 'add-product.html')
-
-def order(request):
-      return render(request, 'order-status.html')
